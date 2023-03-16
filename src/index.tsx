69617d0985332944309be211bfafdda00947b278
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-16 21:26:08 +0200

    fix: add skip rules

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AppContextProvider } from './utils/storage';
-// import { CommandOutputContext } from './utils/storage';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
@@ -14,5 +12,3 @@ root.render(
     </AppContextProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
