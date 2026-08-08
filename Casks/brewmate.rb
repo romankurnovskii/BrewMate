63eccce0be23bcc7fafa87cc405d8a1c719e3663
R K <email@example.com>
2026-08-08 07:48:39 +0000

    chore: update cask to version 1.0.39 [skip ci]

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -1,11 +1,11 @@
 cask "brewmate" do
-  version '1.0.38'
+  version '1.0.39'
 
   url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal.dmg"
   name "BrewMate"
   desc "Homebrew GUI apps manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
-  sha256 'fb3200f79150d783f782d2f62a17e638d499ae3ef28e94da1ef04b8c7d37aef4'
+  sha256 '88e62e9f7ab508b5bcd47e87923eccf2e5b8d3d2a3e95546fe14ea625dabcb16'
 
   auto_updates true
 
